f0d182aa1940659d97dd5f5106d885cbccaac183
Chi Wing Ng <cwng@cern.ch>
2021-03-15 23:15:58 -0400

    Change to cmake compilation

--- HW4/autorun.sh
+++ HW4/autorun.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+outputpath=$1
 sizes=(small large)
 models=(1 2)
+
 for size in "${sizes[@]}";
 do
+    mkdir -p $outputpath/${size}output/
+    
     if [ $size = "small" ]; then
         n_epoch=30
     else
@@ -13,11 +17,14 @@ do
     for m in "${models[@]}";
     do
         echo Running $size with model $m and n_epochs = $n_epoch
-        ./feature handout/${size}data/train_data.tsv handout/${size}data/valid_data.tsv handout/${size}data/test_data.tsv handout/dict.txt myresults/${size}output/model${m}_formatted_train.tsv myresults/${size}output/model${m}_formatted_valid.tsv myresults/${size}output/model${m}_formatted_test.tsv $m
-        ./lr myresults/${size}output/model${m}_formatted_train.tsv myresults/${size}output/model${m}_formatted_valid.tsv myresults/${size}output/model${m}_formatted_test.tsv handout/dict.txt myresults/${size}output/model${m}_train_out.labels myresults/${size}output/model${m}_test_out.labels myresults/${size}output/model${m}_metrics_out.txt $n_epoch
-        echo "My results : "
-        cat myresults/${size}output/model${m}_metrics_out.txt
-        echo "Should match : "
-        cat handout/${size}output/model${m}_metrics_out.txt
+        ./build/Feature handout/${size}data/train_data.tsv handout/${size}data/valid_data.tsv handout/${size}data/test_data.tsv handout/dict.txt $outputpath/${size}output/model${m}_formatted_train.tsv $outputpath/${size}output/model${m}_formatted_valid.tsv $outputpath/${size}output/model${m}_formatted_test.tsv $m
+        ./build/Lr $outputpath/${size}output/model${m}_formatted_train.tsv $outputpath/${size}output/model${m}_formatted_valid.tsv $outputpath/${size}output/model${m}_formatted_test.tsv handout/dict.txt $outputpath/${size}output/model${m}_train_out.labels $outputpath/${size}output/model${m}_test_out.labels $outputpath/${size}output/model${m}_metrics_out.txt $n_epoch
+        echo "Comparing results : "
+        output=$(diff -c $outputpath/${size}output/model${m}_metrics_out.txt handout/${size}output/model${m}_metrics_out.txt)
+        if [ -z "$output" ]; then
+            echo "The result matches expectation!"
+        else
+            echo $output
+        fi
     done
 done
